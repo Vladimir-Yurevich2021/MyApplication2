f0a4d1937c0c2bec44dc15a8d940f4cf70a2f9a9
Vladimir_Yurevich <dvagig@gmail.com>
2021-11-26 20:02:58 +0300

    создали новый класс для заметок

---
+++ app/src/main/java/com/example/myapplication2/Note.java
@@ -0,0 +1,14 @@
+package com.example.myapplication2;
+
+public class Note {
+    private String nameNote;
+    private String contentNote;
+    private int dataNote;
+   Note(String nameNote,String contentNote,int dataNote){
+       this.nameNote=nameNote;
+       this.contentNote=contentNote;
+       this.dataNote=dataNote;
+   }
+
+
+}
