580efd514e83a06677cad484a41ee050f932e9a9
Vladimir_Yurevich <dvagig@gmail.com>
2021-12-06 21:43:09 +0300

    подключили в активити боковое меню

--- app/src/main/java/com/example/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.myapplication2;
 
 import android.content.res.Configuration;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.drawerlayout.widget.DrawerLayout;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+initNavigationView();
 
         if (getResources().getConfiguration().orientation ==
                 Configuration.ORIENTATION_LANDSCAPE) {
@@ -26,5 +30,25 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
+    }
+
+    private void initNavigationView() {
+
+        NavigationView navigationView=findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id=item.getItemId();
+                switch (id){
+                    case R.id.drawer_about:
+                       // openAboutFragment();
+                        return true;
+                    case R.id.drawer_exit:
+                        finish();
+                        return true;
+
+                }                return false;
+            }
+        });
     }
 }
