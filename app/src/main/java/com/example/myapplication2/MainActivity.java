25a788c48c47de76195dd464f5d656176516b47e
Vladimir_Yurevich <dvagig@gmail.com>
2021-12-09 21:10:53 +0300

    Изменили SnackBar на диалог Alert, и сделали выход из приложения с отображением Toast уведомления

--- app/src/main/java/com/example/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.myapplication2;
 
+import android.content.DialogInterface;
 import android.content.res.Configuration;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -17,14 +19,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-initNavigationView();
+        initNavigationView();
 
         if (getResources().getConfiguration().orientation ==
                 Configuration.ORIENTATION_LANDSCAPE) {
             getSupportFragmentManager()
                     .beginTransaction()
                     .replace(R.id.txt_note_result,
-                           NotesFragment.newInstance(0)).commit();
+                            NotesFragment.newInstance(0)).commit();
         } else {
             getSupportFragmentManager()
                     .beginTransaction()
@@ -37,43 +39,47 @@ initNavigationView();
 
     private void initNavigationView() {
 
-        NavigationView navigationView=findViewById(R.id.navigation_view);
+        NavigationView navigationView = findViewById(R.id.navigation_view);
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int id=item.getItemId();
-                switch (id){
+                int id = item.getItemId();
+                switch (id) {
                     case R.id.drawer_about:
                         openAboutFragment();
                         return true;
                     case R.id.drawer_exit:
-                        Snackbar.make(findViewById(R.id.navigation_view),"Вы уверены, что хотите выйти?",Snackbar.LENGTH_INDEFINITE)
-                                        .setAction("Выйти", new View.OnClickListener() {
-                                            @Override
-                                            public void onClick(View view) {
-                                                showToast();
-                                            }
-                                        }).show();
+                        new AlertDialog.Builder(MainActivity.this)
+                                .setTitle("Предупреждение!")
+                                .setMessage("Вы собираетесь выйти из программы?")
+                                .setPositiveButton("Да", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        showToast();
+                                        finish();
+                                    }
+                                }).setNeutralButton("Отмена", null).show();
 
-                       // finish();
+
                         return true;
 
-                }                return false;
+                }
+                return false;
             }
         });
     }
 
     private void showToast() {
         Toast.makeText(this, "Спасибо, что пользуетесь нашим приложением!", Toast.LENGTH_SHORT).show();
-    
+
     }
 
     private void openAboutFragment() {
-        AboutFragment aboutFragment=new AboutFragment();
+        AboutFragment aboutFragment = new AboutFragment();
         getSupportFragmentManager()
                 .beginTransaction()
                 .addToBackStack("")
-                .replace(R.id.container_fragment,aboutFragment)
+                .replace(R.id.container_fragment, aboutFragment)
                 .commit();
     }
 }
